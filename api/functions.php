55103aaf6fcb4d6c7a75ed2d993e3fa94e02dcc5
Zarxong <89886162+Zarxong@users.noreply.github.com>
2022-04-04 13:41:44 -0400

    Create functions.php

---
+++ api/functions.php
@@ -0,0 +1,15 @@
+<?php
+function returnWithError( $err ) {
+   		$retValue = '{"error":"' . $err . '"}';
+   		sendResultInfoAsJson( $retValue );
+}
+
+function sendResultInfoAsJson( $obj ) {
+   		header('Content-type: application/json');
+   		echo $obj;
+}
+
+function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
